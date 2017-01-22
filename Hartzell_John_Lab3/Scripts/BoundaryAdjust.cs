bac6e1d898f89f6282133971f59c46db7dbf20c6
AngrySpartan <ssmith2@fullsail.edu>
2017-01-22 14:28:24 -0500

    Add files via upload

---
+++ Hartzell_John_Lab3/Scripts/BoundaryAdjust.cs
@@ -0,0 +1,66 @@
+﻿//------------------------------------------------------------------------------------------------
+// Author: John Hartzell
+// Date: 7/22/16
+// Credit: PGF2_Experiment01 https://assethub.fso.fullsail.edu/assethub/PGF2_Comments_d9e9fe80-a238-40e9-9b3e-40646e6765cb.pdf
+//
+// Purpose: This script is used to adjust the horizontal boundaries when the camera size changes.
+//
+
+using UnityEngine;
+using System.Collections;
+
+
+
+public class BoundaryAdjust : MonoBehaviour {
+
+    [Tooltip("Adjust this so that the boundaries are at the edge of the screen.")]
+    [Range(-500,250)]
+    [SerializeField]
+    private int screenOffset;
+
+    [SerializeField]
+    [Tooltip("Is the boundary the left boundary?")]
+    public bool leftBoundary = true;
+
+    // place the main camera in this object. It will be needed to calculate where to put the boundaries.
+    public Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateBoundary();
+	
+	}
+
+    private void UpdateBoundary()
+    {
+        Vector3 screenPosition = mainCamera.WorldToScreenPoint(
+            gameObject.transform.position);
+
+        // Is the boundary the left, true, or right boundary?
+        if (leftBoundary == false)
+        {
+            // Get the width of the screen and set the x position to the value.
+            // This will result in the boundary being sent to the right side of the screen.
+            // Adjust the offset so the boundary is at just the edge of the camera.
+            screenPosition.x = Screen.width + screenOffset;
+            // Set the boundary to that position.
+            gameObject.transform.position =
+                mainCamera.ScreenToWorldPoint(screenPosition);
+        }
+        else if (leftBoundary == true) 
+        {
+            // Set the x position to 0, the farthest left side of the screen.
+            // This will result in the boundary being sent to the left side of the screen.
+            // Adjust the offset so the boundary is at just the edge of the camera. 
+            screenPosition.x = 0 - screenOffset;
+            // Set the boundary to that position.
+            gameObject.transform.position =
+                mainCamera.ScreenToWorldPoint(screenPosition);
+        }
+    }
+}
---
+++ Hartzell_John_Lab3/Scripts/PlayerScreenWrap.cs
@@ -0,0 +1,145 @@
+﻿
+//------------------------------------------------------------------------------------------------
+// Author: John Hartzell
+// Date: 7/22/16
+// Credit: PGF2_Experiment01 https://assethub.fso.fullsail.edu/assethub/PGF2_Comments_d9e9fe80-a238-40e9-9b3e-40646e6765cb.pdf
+// Credit: The camera shake methods I use are inspired by the example given on this site:
+// http://newbquest.com/2014/06/the-art-of-screenshake-with-unity-2d-script/
+// The code on the site was inspired by a presentation given by one of the developers of nuclear throne.
+// The author of the blog post is Matt Mirrorfish. The author credits these posts partially with the creation of 
+// the example code they provide: http://answers.unity3d.com/questions/220407/damaging-a-car-depending-on-force-of-impact.html
+// http://answers.unity3d.com/questions/46317/screen-shake-effect.html?sort=oldest
+//
+// Purpose: This script is suppose to wrap the player around to the other side of the screen
+// when they exit out a horizontal boundary. This script also produces camera shake when the player touches the 
+// horizontal boundaries.
+//
+
+using UnityEngine;
+using System.Collections;
+
+
+public class PlayerScreenWrap : MonoBehaviour
+{
+    // Is this the left boundary?
+    [Tooltip("Is this the left boundary?")]
+    [SerializeField]
+    bool isLeft = false;
+
+    [Tooltip("Adjust the camera shake amount.")]
+    [Range(0, 0.1f)]
+    [SerializeField]
+    float shakeModifier = 0.1f;
+
+    private float tempShakeModifier = 0.0f;
+
+    // Used to determine screen bounds in relation to the player object.
+    public Camera cameraObject;
+
+    private Vector3 cameraPosition;
+
+    public void Start()
+    {
+        tempShakeModifier = shakeModifier;
+        cameraPosition = cameraObject.transform.position;
+        
+    }
+
+    // When the player enters the boundary, which is positioned just off screen.
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+
+            // Transform the 3D world position to a screen pixel location
+            Vector3 screenPosition = cameraObject.WorldToScreenPoint(
+                other.gameObject.transform.position);
+
+            if (isLeft == true)
+            {
+                // set to the screen's right side
+                screenPosition.x = Screen.width - 15;
+                // set the players new position
+                other.gameObject.transform.position =
+                cameraObject.ScreenToWorldPoint(screenPosition);
+
+            }
+            else
+            {
+                // set to the screen's left side
+                screenPosition.x = 0 + 15;
+                // set the players new position
+                other.gameObject.transform.position =
+                cameraObject.ScreenToWorldPoint(screenPosition);
+
+            }
+            // invoke the camera to start shaking every .01 seconds.
+            // invoke stop camera shake after .3 seconds.
+            InvokeRepeating("StartCameraShake", 0.0f, 0.005f);
+            Invoke("StopCameraShake", 0.3f);
+
+        }
+    }
+
+    // shakes the camera
+    void StartCameraShake()
+    {
+        // randomly choose between 1 (inclusive) and 5 (exclusive). This will determine which
+        // direction the camera will shake in.
+        int randNum = Random.Range(1,5);
+
+        // if the shake modifier is greater than 0.
+        if(shakeModifier > 0)
+        {
+            // the offset generation formula. 
+            shakeModifier = Random.value * shakeModifier * 2 - shakeModifier;
+
+            // set the temporary position to the camera's current position.
+            Vector3 tempCameraPosition = cameraObject.transform.position;
+
+            //shake the camera by applying the offset. The number randomly generated above 
+            // determines the direction of the shake.
+            if (randNum == 1) {
+                //shake the camera by applying the offset.
+                tempCameraPosition.y += shakeModifier;
+                tempCameraPosition.x -= shakeModifier;
+
+                cameraObject.transform.position = tempCameraPosition;
+            }
+            else if (randNum == 2)
+            {
+                //shake the camera by applying the offset.
+                tempCameraPosition.y -= shakeModifier;
+                tempCameraPosition.x += shakeModifier;
+
+                cameraObject.transform.position = tempCameraPosition;
+            }
+            else if(randNum == 3)
+            {
+                //shake the camera by applying the offset.
+                tempCameraPosition.y -= shakeModifier;
+                tempCameraPosition.x -= shakeModifier;
+
+                cameraObject.transform.position = tempCameraPosition;
+            }
+            else
+            {
+                //shake the camera by applying the offset.
+                tempCameraPosition.y += shakeModifier;
+                tempCameraPosition.x += shakeModifier;
+
+                cameraObject.transform.position = tempCameraPosition;
+            }
+        }
+    }
+
+    void StopCameraShake()
+    {
+        //cancel the camera shake and return the camera to its normal position.
+        CancelInvoke("StartCameraShake");
+        cameraObject.transform.position = cameraPosition;
+
+        //reset the shake modifier so that the camera will properly shake again.
+        shakeModifier = tempShakeModifier;
+    }
+}
